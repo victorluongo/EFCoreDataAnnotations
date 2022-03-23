d9faf82974606b21b986d8ec8c7771e63d74d2bb
Victor Luongo <victor.luongo@gmail.com>
2022-03-23 15:12:31 -0300

    Initial Project Configuration

---
+++ EFCoreDataAnnotations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreDataAnnotations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
