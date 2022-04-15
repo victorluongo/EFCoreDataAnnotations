c41ea31326668592100ad6230177868ec3723ce4
Victor Luongo <victor.luongo@gmail.com>
2022-04-15 09:32:50 -0300

    Table Attributes

--- EFCoreDataAnnotations/Models/Flight.cs
+++ EFCoreDataAnnotations/Models/Flight.cs
@@ -1,14 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EFCoreDataAnnotations.Models
 {
+    [Table("Flights")]
     public class Flight
     {
+        [Key]
         public int Id {get; set;}
+        
+        [Required]
+        [Column("FlightNumber", TypeName = "VARCHAR(6)")]
         public string Number {get; set;}
+        
+        [MaxLength(150)]
         public string Description {get; set;}
     }
 }
